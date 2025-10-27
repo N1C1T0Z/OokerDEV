906cf75fb040dda6c89d0020a8fd2b487e470b19
N1C1T0Z <inartpro2024@gmail.com>
2025-10-27 14:23:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,9 +131,15 @@ def nova():
     return render_template('nova.html', is_admin=is_admin, projects=projects)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
 
