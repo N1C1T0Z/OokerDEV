75755d141d51077287873056ef359691026e42de
N1C1T0Z <inartpro2024@gmail.com>
2025-10-30 11:09:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -618,6 +618,6 @@ def api_login():
 # ----------------------------
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    debug_mode = False
+    debug_mode = True
     print(f"App démarrée. Stockage distant : {REMOTE_STORAGE_BASE} (clé fournie).")
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
